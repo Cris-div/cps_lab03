ca95a2fd76056a5531b67403e70985df3214f691
Cris-div <christian.unocc@tecsup.edu.pe>
2025-09-04 14:35:01 -0500

    segundo commit

--- src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
+++ src/main/java/pe/edu/tecsup/lab03/controllers/StudentController.java
@@ -1,12 +1,4 @@
 package pe.edu.tecsup.lab03.controllers;
 
 public class StudentController {
-    public void greet() {
-        //Metodo vacio
-    }
-
-    public static void main(String[] args) {
-        StudentController controller = new StudentController();
-        controller.greet();
-    }
 }
